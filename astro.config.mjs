ee26fcfdd897287b6a4a26b0ee17ed1c503944b5
Cleyrol Santana <77045476+cleyrolsc@users.noreply.github.com>
2025-07-29 15:11:36 -0400

    Add custom domain to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pipxlabs.github.io',
+  site: 'https://pipxlabs.com',
   build: {
     // @ts-ignore
     outDir: './docs',
