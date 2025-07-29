fc8d21cc09000ac43d4c50225d86918831079c30
Cleyrol Santana <77045476+cleyrolsc@users.noreply.github.com>
2025-07-29 01:48:54 -0400

    Add github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'pipxlabs.github.io',
   vite: {
     // @ts-ignore
     plugins: [tailwindcss()]
